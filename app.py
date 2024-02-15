58f45d250dab5cdb5289c13bb0fd1d9d3efa3479
James Bryant <jamesbryant792@gmail.com>
2024-02-15 16:58:43 +0000

    updated

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 print("I am the app from the test repo")
+
+
+def my_function():
+    print("I am 'my_function' from the app in the test repo")
+    return "test_return"
+
+if __name__ == '__main__':
+    my_function()
