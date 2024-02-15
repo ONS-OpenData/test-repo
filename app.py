40e00354636da3ee9a4c08a0fcc9ef6779e204c4
James Bryant <jamesbryant792@gmail.com>
2024-02-15 17:01:28 +0000

    tidying

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-print("I am the app from the test repo")
-
-
 def my_function():
     print("I am 'my_function' from the app in the test repo")
-    return "test_return"
+    return 
 
 if __name__ == '__main__':
     my_function()
