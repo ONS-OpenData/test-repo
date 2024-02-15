4d00d121018bcca466fff1159b71c10777ee58a8
James Bryant <jamesbryant792@gmail.com>
2024-02-15 17:06:02 +0000

    updated

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
+import os
+
 def my_function():
     print("I am 'my_function' from the app in the test repo")
     return 
 
+def get_working_directory():
+    return os.getcwd()
+
+
 if __name__ == '__main__':
+    print("Running my_function()")
     my_function()
+
+    print("Running get_working_directory")
+    wd = get_working_directory()
+    print(wd)
